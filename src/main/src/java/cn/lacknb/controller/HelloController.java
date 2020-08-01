65a38f9be8143cf90b7e6ac9d9eea40245719b49
nianshao <gitsilence@lacknb.cn>
2020-08-01 14:24:39 +0800

    修改部分

--- src/main/src/java/cn/lacknb/controller/HelloController.java
+++ src/main/src/java/cn/lacknb/controller/HelloController.java
@@ -1,17 +1,15 @@
 package cn.lacknb.controller;
 
-import cn.lacknb.dao.UserDao;
 import cn.lacknb.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/index")
 public class HelloController {
 
@@ -19,14 +17,14 @@ public class HelloController {
     private UserService userService;
 
     @GetMapping("/hello")
-    @ResponseBody
     public String hello () {
         return "Hello SSM ";
     }
 
     @GetMapping("/list")
-    @ResponseBody
     public Map<String, Object> find () {
+
+
         Map<String, Object> map = new HashMap<>();
         map.put("code", 200);
         map.put("datas", userService.selectList());
